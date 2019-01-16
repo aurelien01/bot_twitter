65f93d0b24b1afb2c8c059869bfdddf462aa9dcd
aurelien <duquenneaurelien@gmail.com>
2019-01-16 15:08:00 +0100

    test

--- lib/bot_twitter.rb
+++ lib/bot_twitter.rb
@@ -3,14 +3,18 @@ require 'dotenv'
 
 Dotenv.load
 
-
-# quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+return client
 end
 
-# ligne qui permet de tweeter sur ton compte
-client.update('Mon premier tweet en Ruby !!!!')
+
+
+login_twitter.update ('road to influenceur')
+
+
